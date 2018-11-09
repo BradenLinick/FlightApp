1352f723689650e7b2f3421392bc1ca5a3cbc67f
kybernetic <kybernetic@tuta.io>
2018-11-09 11:35:36 +0100

    flightlogic

--- src/flights/flights.jsx
+++ src/flights/flights.jsx
@@ -13,19 +13,29 @@ export default class Flights extends React.Component {
 
     }
 
-    componentDidMount() {
-        let newArray = [];        
+    componentDidMount() {        
         fetch("https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2018&dateTo=12/12/2018")
             .then(resp => resp.json())
-            .then(function (resp) {
-                resp.forEach(element => {
-                    console.log(element);
-                    newArray.push(element);
-                })
-            })
-            .then(() => this.setState({flightsArray: newArray}))
-            console.log(flightsArray);
+            .then(json => {
+                console.log(json.data);
+                this.setState({flightsArray: json.data})             
+            });
     }
+
+    
+    /* componentDidMount() {        
+        fetch(`https://api.skypicker.com/flights?flyFrom=${this.props.flyFrom}&to=${this.props.to}&dateFrom=${this.props.dateFrom}&dateTo=${this.props.dateTo}`)
+            .then(resp => resp.json())
+            .then(json => {
+                console.log(json.data);
+                this.setState({flightsArray: json.data})             
+            });
+    } */
+
+    handleChange(event) {
+        this.setState({flightSearch: event.target.value})
+        console.log(flightSearch);
+      }
 
 
 
@@ -34,9 +44,21 @@ export default class Flights extends React.Component {
 
 
     render() {
-        return (
+        return(
+            <div className="flightsTable">
 
+                   <input type="text" name="flightSearch" value={this.state.title} onChange={this.handleChange.bind(this)}/>
+
+
+                
+                    {this.state.flightsArray.map(flights => <p> {flights.flyFrom} </p>)}
+              
+                    {this.state.flightsArray.map(flights => <p> {flights.flyTo} </p>)}
+                
+
+            </div>
         )
+        
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import { render } from 'react-dom';
 import Load from './loader/loader.jsx';
 import './index.css';
 import './index.html';
+import Flights from './flights/flights.jsx';
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div>
         <h1>Hello React!</h1>
         
         <Load />
+        <Flights />
         
-      </>
+      </div>
     )
   }
 }
